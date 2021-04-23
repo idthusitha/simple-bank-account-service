048013fcb82966507eb9f642f6021750c8c01354
thusitha <tdissanayaka@accelaero.com>
2021-04-23 12:50:31 +0200

    security changes

--- src/main/java/com/bank/account/simplebankaccountservice/configuration/SecurityConfig.java
+++ src/main/java/com/bank/account/simplebankaccountservice/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bank.account.simplebankaccountservice.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +9,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Value("${admin.user.name}")
+	private String userName;
+	
+	@Value("${admin.password}")
+	private String password;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.csrf().disable()
+		.authorizeRequests()		
+		.anyRequest()		
+		.authenticated()
+		.and()
+		.httpBasic();
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("USER");
+		auth.inMemoryAuthentication().withUser(userName).password("{noop}"+password).roles("ADMIN");
 	}
 }
--- src/main/java/com/bank/account/simplebankaccountservice/configuration/SwaggerConfig.java
+++ src/main/java/com/bank/account/simplebankaccountservice/configuration/SwaggerConfig.java
@@ -4,16 +4,27 @@ import static com.google.common.base.Predicates.and;
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,12 +37,15 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.SWAGGER_2)				
 				.select()
 				.apis(RequestHandlerSelectors.any())
+				
 				.paths(paths())
 				.build()
-				.apiInfo(apiInfo());
+				.apiInfo(apiInfo())
+				.forCodeGeneration(true)
+				.genericModelSubstitutes(ResponseEntity.class);
 	}	
 	 
 	
